9b1d1367b781a24102f56e1fbb9fbe1f386a04c8
cfontelar <140099828+cfontelar@users.noreply.github.com>
2023-11-22 09:09:44 -0600

    User Registration Form

--- register.php
+++ register.php
@@ -1,12 +1,56 @@
 <?php
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-include("connect.php"); 
+define('DB_DSN', 'mysql:host=localhost;dbname=svpbdatabase;charset=utf8');
+define('DB_USER', 'serveruser');
+define('DB_PASS', 'fontelar');
 
+try {
+    // Try creating a new PDO connection to MySQL.
+    $db = new PDO(DB_DSN, DB_USER, DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+    die();
+}
 
+// Initialize variables
+$username = $email = $password = $confirm_password = '';
+$registration_status = ''; // Variable to store registration status message
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Check if all required fields are set
+    if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirm_password'])) {
+        // Sanitize user input to prevent SQL injection
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
+        $confirm_password = htmlspecialchars($_POST['confirm_password']);
+
+        // Validate passwords match
+        if ($password === $confirm_password) {
+            // Hash the password before storing it in the database
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            // Prepare and execute the SQL query to insert a new user
+            $stmt = $db->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+            $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $hashed_password, PDO::PARAM_STR);
+
+            if ($stmt->execute()) {
+                $registration_status = "<p class='success-message'>User registered successfully!</p>";
+            } else {
+                $registration_status = "<p class='error-message'>Error registering user!</p>";
+            }
+        } else {
+            $registration_status = "<p class='error-message'>Passwords do not match!</p>";
+        }
+    } else {
+        $registration_status = "<p class='error-message'>All fields are required!</p>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -44,27 +88,32 @@ include("connect.php");
         </nav>
     </header>
 
-    <!-- Register Form -->
-    <div class="register-container">
+     <!-- Register Form -->
+     <div class="register-container">
         <h2>Register</h2>
-        <?php 
-      
-        ?>
+
+        <!-- Registration Status Message Box -->
+        <div class="registration-status-box">
+            <?php echo $registration_status; ?>
+        </div>
+
+        <!-- Registration Form -->
         <form action="register.php" method="post">
-            
             <label for="username">Username:</label>
-            <input type="text" name="username" required>
-            
+            <input type="text" name="username" required value="<?php echo $username; ?>">
+
+            <label for="email">Email:</label>
+            <input type="email" name="email" required value="<?php echo $email; ?>">
+
             <label for="password">Password:</label>
             <input type="password" name="password" required>
-            
+
             <label for="confirm_password">Confirm Password:</label>
             <input type="password" name="confirm_password" required>
-            
-            
 
             <button type="submit">Register</button>
         </form>
+
         <p>Already have an account? <a href="login.php">Login</a></p>
     </div>
 
